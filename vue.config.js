e0a70ab3c84b2d6c4af7e4abc5e8c6cdf4f6df85
joonpeter <68453765+joonpeter@users.noreply.github.com>
2026-01-01 16:11:59 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -16,8 +16,8 @@ module.exports = {
 
 // vue.config.js
 module.exports = {
-	  lintOnSave: false,//yjsohn
   transpileDependencies: ['vuetify'],
+lintOnSave: false,//yjsohn
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Board'
